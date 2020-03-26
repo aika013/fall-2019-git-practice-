bbb03dd12e467441d399ded220fed018b27c053a
Aiganysh Tukueva <aiganyshtukueva@Aiganyshs-MacBook-Pro.local>
2020-03-25 20:12:31 -0400

    4 changes

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,6 +1,10 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("brave new world");
+        System.out.println("brave new world"); // comment
+        System.out.println("someone");
+        System.out.println("change one");
+        System.out.println("change two");
+        System.out.println("change three");
         //first test of pull from git hub
         
         //another change.  // one ore change
